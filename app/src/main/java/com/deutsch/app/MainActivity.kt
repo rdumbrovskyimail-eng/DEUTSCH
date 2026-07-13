18fcff0afaab73c62863b7355b2090ed9a65f917
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-07-14 00:55:10 +0200

    [Pipeline] MainActivity.kt

--- app/src/main/java/com/deutsch/app/MainActivity.kt
+++ app/src/main/java/com/deutsch/app/MainActivity.kt
@@ -3,13 +3,8 @@ package com.deutsch.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import com.deutsch.app.ui.AnalyzerScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
